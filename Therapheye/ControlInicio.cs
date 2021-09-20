df316b849d862e035129337e0d40a7ec742b4ea5
Genaro <genaro.gonzalez16@tectijuana.edu.mx>
2021-09-20 16:04:13 -0700

    Subida Proyecto Residencia

---
+++ Therapheye/ControlInicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Therapheye
+{
+    public partial class ControlInicio : UserControl
+    {
+        public ControlInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void ControlInicio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
